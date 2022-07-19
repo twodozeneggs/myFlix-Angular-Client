f2129dcafa1ffd7aad6e4b812e241c3311bc9f62
twodozeneggs <simonjohnloik@gmail.com>
2022-07-19 14:52:16 -0500

    6.3 updates
    html files in user-login and user-registration have a couple of weird problems.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
+
+
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
+
+  constructor(
+    public dialog: MatDialog
+  ) { }
+
+  /**
+   * opens the user registration dialog when signup button is clicked
+   */
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      // Assign dialog width
+      width: '280px'
+    });
+  }
+
+  /**
+   * opens the user login dialog when login button is clicked
+   */
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      // Assign dialog width
+      width: '280px'
+    });
+  }
 }
